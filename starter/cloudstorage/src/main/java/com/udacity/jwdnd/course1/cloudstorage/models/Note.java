1b04a52501b4fe4cfe9bbf2903e564213a23e7e4
NMQ <ngminhquan.aries@gmail.com>
2024-07-05 23:02:30 +0700

    Create models (Credential, File, Note, User)

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/Note.java
@@ -0,0 +1,50 @@
+package com.udacity.jwdnd.course1.cloudstorage.models;
+
+public class Note {
+    private int noteid;
+    private String notetitle;
+    private String notedescription;
+    private int userid;
+
+    public Note(int noteid, String notetitle, String notedescription, int userid) {
+        this.noteid = noteid;
+        this.notetitle = notetitle;
+        this.notedescription = notedescription;
+        this.userid = userid;
+    }
+
+    public Note() {
+    }
+
+    public int getNoteid() {
+        return noteid;
+    }
+
+    public void setNoteid(int noteid) {
+        this.noteid = noteid;
+    }
+
+    public String getNotetitle() {
+        return notetitle;
+    }
+
+    public void setNotetitle(String notetitle) {
+        this.notetitle = notetitle;
+    }
+
+    public String getNotedescription() {
+        return notedescription;
+    }
+
+    public void setNotedescription(String notedescription) {
+        this.notedescription = notedescription;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+}
---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/User.java
@@ -0,0 +1,70 @@
+package com.udacity.jwdnd.course1.cloudstorage.models;
+
+public class User {
+    private int userid;
+    private String username;
+    private String password;
+    private String salt;
+    private String firstname;
+    private String lastname;
+
+    public User(int userid, String username, String password, String salt, String firstname, String lastname) {
+        this.userid = userid;
+        this.username = username;
+        this.password = password;
+        this.salt = salt;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public User() {
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
